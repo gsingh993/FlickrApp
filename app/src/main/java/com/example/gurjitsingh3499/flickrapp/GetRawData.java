f2dd826c374da88811d94ee6cb7bef62c78341a0
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-10 17:01:56 -0800

    Downloading Data

--- app/src/main/java/com/example/gurjitsingh3499/flickrapp/GetRawData.java
+++ app/src/main/java/com/example/gurjitsingh3499/flickrapp/GetRawData.java
@@ -1,6 +1,14 @@
 package com.example.gurjitsingh3499.flickrapp;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 enum DownloadStatus {IDLE, PROCESSING, NOT_INITIALIZED, FAILED_OR_EMPTY, OK }
 
@@ -12,5 +20,43 @@ class GetRawData extends AsyncTask<String, Void, String> {
 
     public GetRawData() {
         mDownloadStatus = DownloadStatus.IDLE;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        if(strings == null){
+            mDownloadStatus = DownloadStatus.NOT_INITIALIZED;
+            return null;
+        }
+        try{
+            mDownloadStatus = DownloadStatus.PROCESSING;
+            URL url = new URL(strings[0]);
+
+            connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int response = connection.getResponseCode();
+            Log.d(TAG, "doInBackground: the response"+response);
+
+            StringBuilder result = new StringBuilder();
+
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        } catch(MalformedURLException e){
+            Log.e(TAG, "doInBackground: Invalid URL"+ e.getMessage());
+        } catch(IOException e){
+            Log.e(TAG, "doInBackground: IO exception reading data" + e.getMessage());
+        } catch(SecurityException e){
+            Log.e(TAG, "doInBackground: Security Exception" + e.getMessage() );
+        }
+        return null;
     }
 }
