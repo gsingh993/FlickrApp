feda87785f67b33f248577232921bb3cfe862457
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-10 16:36:14 -0800

    adding data

---
+++ app/src/main/java/com/example/gurjitsingh3499/flickrapp/GetRawData.java
@@ -0,0 +1,11 @@
+package com.example.gurjitsingh3499.flickrapp;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by gurjitsingh on 2/10/18.
+ */
+
+class GetRawData extends AsyncTask<String, Void, String> {
+
+}
