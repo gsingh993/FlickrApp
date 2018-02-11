7377896bf823c763640e7181d6135e5437e97e49
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-10 16:45:07 -0800

    Adding Enums

--- app/src/main/java/com/example/gurjitsingh3499/flickrapp/GetRawData.java
+++ app/src/main/java/com/example/gurjitsingh3499/flickrapp/GetRawData.java
@@ -2,10 +2,15 @@ package com.example.gurjitsingh3499.flickrapp;
 
 import android.os.AsyncTask;
 
-/**
- * Created by gurjitsingh on 2/10/18.
- */
+enum DownloadStatus {IDLE, PROCESSING, NOT_INITIALIZED, FAILED_OR_EMPTY, OK }
+
 
 class GetRawData extends AsyncTask<String, Void, String> {
+    private static final String TAG = "GetRawData";
 
+    private DownloadStatus mDownloadStatus;
+
+    public GetRawData() {
+        mDownloadStatus = DownloadStatus.IDLE;
+    }
 }
